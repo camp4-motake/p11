f6eb260bfeda7a6e41afeb9589b870dfc5db7f7f
motake <munetoshi_otake@camp4.jp>
2023-11-30 14:25:28 +0900

    update structure

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import intersect from "@alpinejs/intersect"
 import Alpine from "alpinejs"
 import { components } from "./components"
 import { stores } from "./stores"
-import "./main.css"
-
-// main
 ;(async () => {
   window.Alpine = Alpine
   Alpine.plugin(intersect)
