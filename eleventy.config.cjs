5211ca5a30f65f20c4d7b1610d1cea328f3b19e9
motake <munetoshi_otake@camp4.jp>
2023-11-30 19:54:53 +0900

    tree shake

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,8 +1,8 @@
 module.exports = function (/* eleventyConfig */) {
   return {
     dir: {
-      input: `${process.env.npm_package_config_input}pages`,
-      output: process.env.npm_package_config_intermediate,
+      input: "src/pages",
+      output: "dist",
       includes: "../components",
       layouts: "../layouts",
       data: "../data",
