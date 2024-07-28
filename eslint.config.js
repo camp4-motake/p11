771fdab98831b4520e306ae1dccf7a0cbbe6cab4
motake <munetoshi_otake@camp4.jp>
2024-07-28 18:05:22 +0900

    fix config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,56 @@
 import pluginJs from "@eslint/js"
+import tsPlugin from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
 import eslintConfigPrettier from "eslint-config-prettier"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
 import globals from "globals"
-import tsEslint from "typescript-eslint"
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { ignores: ["dist/**/*", "node_modules/**/*", "vendor/**/*", "_site/**/*"] },
   pluginJs.configs.recommended,
-  ...tsEslint.configs.recommended,
-  eslintConfigPrettier,
   {
     plugins: {
       "simple-import-sort": simpleImportSort,
     },
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     rules: {
       "no-console": ["error", { allow: ["warn", "error"] }],
-      "no-unused-vars": "off",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
     },
   },
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  {
+    files: ["**/*.cjs"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      ...tsPlugin.configs["recommended"].rules,
+    },
+  },
+  eslintConfigPrettier,
 ]
