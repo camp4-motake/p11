7a3146eb8e9b5fa7fae8d69b83a343d5135a03b1
motake <munetoshi_otake@camp4.jp>
2024-07-31 11:26:46 +0900

    fix formater

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,27 +1,26 @@
-import "tsx/esm"
+import 'tsx/esm';
 
-import path from "node:path"
+import path from 'node:path';
 
-import sizeOf from "image-size"
-import { renderToStaticMarkup } from "react-dom/server"
+import sizeOf from 'image-size';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 export default function (eleventyConfig) {
   /**
    * jsx support
    * @see https://www.11ty.dev/docs/languages/jsx/
    */
-  eleventyConfig.addExtension(["11ty.jsx", "11ty.ts", "11ty.tsx"], {
-    key: "11ty.js",
-    compile: function () {
-      return async function (data) {
-        let content = await this.defaultRenderer(data)
-        return renderToStaticMarkup(content)
-      }
-    },
-  })
-  eleventyConfig.addTransform("tsx", async (content) => {
-    return `<!doctype html>\n${content}`
-  })
+  eleventyConfig.addExtension(['11ty.jsx', '11ty.ts', '11ty.tsx'], {
+    key: '11ty.js',
+    compile: () =>
+      async function (data) {
+        const content = await this.defaultRenderer(data);
+        return renderToStaticMarkup(content);
+      },
+  });
+  eleventyConfig.addTransform('tsx', async (content) => {
+    return `<!doctype html>\n${content}`;
+  });
 
   /**
    * eleventy dev server
@@ -30,64 +29,64 @@ export default function (eleventyConfig) {
   eleventyConfig.setServerOptions({
     domDiff: false,
     showAllHosts: true,
-  })
+  });
 
   /**
    * Ignore Template Files
    * @see https://www.11ty.dev/docs/ignores/
    */
-  eleventyConfig.ignores.add("**/README.md")
-  eleventyConfig.ignores.add("**/.gitkeep")
-  eleventyConfig.ignores.add("**/_drafts/**")
+  eleventyConfig.ignores.add('**/README.md');
+  eleventyConfig.ignores.add('**/.gitkeep');
+  eleventyConfig.ignores.add('**/_drafts/**');
 
   /**
    * Watch Ignores
    * @see https://www.11ty.dev/docs/watch-serve/
    */
-  eleventyConfig.watchIgnores.add("**/*.map")
-  eleventyConfig.watchIgnores.add("**/.**")
+  eleventyConfig.watchIgnores.add('**/*.map');
+  eleventyConfig.watchIgnores.add('**/.**');
 
   /**
    * Passthrough File Copy
    * @see https://www.11ty.dev/docs/copy/
    */
-  eleventyConfig.addPassthroughCopy({})
+  eleventyConfig.addPassthroughCopy({});
 
   /**
    * custom filter
    * @see https://www.11ty.dev/docs/filters/
    */
-  eleventyConfig.addFilter("urlJoin", function (baseUrl = "", pathname = "") {
-    return new URL(path.join(baseUrl, pathname)).toString()
-  })
-  eleventyConfig.addFilter("padStart", function (str = "", length = 2) {
-    return String(str).padStart(length, "0")
-  })
+  eleventyConfig.addFilter('urlJoin', (baseUrl = '', pathname = '') =>
+    new URL(path.join(baseUrl, pathname)).toString(),
+  );
+  eleventyConfig.addFilter('padStart', (str = '', length = 2) =>
+    String(str).padStart(length, '0'),
+  );
 
   /**
    * custom short code
    * @see https://www.11ty.dev/docs/shortcodes/
    */
-  eleventyConfig.addShortcode("img", function (src, attr) {
-    if (!src) return ""
-    const newAttr = getImgAttr(src, attr, "img")
-    return `<img ${newAttr}>`
-  })
-  eleventyConfig.addShortcode("source", function (src, attr) {
-    if (!src) return ""
-    const newAttr = getImgAttr(src, attr, "source")
-    return `<source ${newAttr}>`
-  })
+  eleventyConfig.addShortcode('img', (src, attr) => {
+    if (!src) return '';
+    const newAttr = getImgAttr(src, attr, 'img');
+    return `<img ${newAttr}>`;
+  });
+  eleventyConfig.addShortcode('source', (src, attr) => {
+    if (!src) return '';
+    const newAttr = getImgAttr(src, attr, 'source');
+    return `<source ${newAttr}>`;
+  });
 
   return {
     dir: {
-      input: "src/pages",
-      output: ".tmp",
-      includes: "_includes",
-      layouts: "_includes/layouts",
-      data: "_data",
+      input: 'src/pages',
+      output: '.tmp',
+      includes: '_includes',
+      layouts: '_includes/layouts',
+      data: '_data',
     },
-  }
+  };
 }
 
 /**
@@ -97,60 +96,60 @@ export default function (eleventyConfig) {
  * @param {string} mode
  * @returns {string}
  */
-function getImgAttr(src, attr = {}, mode = "img") {
-  const q = { as: "webp", ...getQueryParamsRegex(src) }
-  const imgSrc = src.split("?")[0]
+function getImgAttr(src, attr = {}, mode = 'img') {
+  const q = { as: 'webp', ...getQueryParamsRegex(src) };
+  const imgSrc = src.split('?')[0];
   const attrType = {
     img: {
-      src: path.join(imgSrc) + `?${objectToQueryString(q)}`,
-      alt: "",
-      loading: "lazy",
+      src: `${path.join(imgSrc)}?${objectToQueryString(q)}`,
+      alt: '',
+      loading: 'lazy',
     },
     source: {
-      srcset: path.join(imgSrc) + `?${objectToQueryString(q)}`,
+      srcset: `${path.join(imgSrc)}?${objectToQueryString(q)}`,
     },
-  }
+  };
 
   if (!attrType[mode]) {
-    return ""
+    return '';
   }
 
-  const imgPath = path.join(__dirname, imgSrc)
-  const size = {}
-  let dimensions = {}
+  const imgPath = path.join(__dirname, imgSrc);
+  const size = {};
+  let dimensions = {};
 
   try {
-    dimensions = sizeOf(imgPath)
-    size.width = dimensions?.width
-    size.height = dimensions?.height
+    dimensions = sizeOf(imgPath);
+    size.width = dimensions?.width;
+    size.height = dimensions?.height;
   } catch (error) {
-    new Error(error)
+    new Error(error);
   }
 
   if (q?.width) {
-    size.width = q?.width
+    size.width = q?.width;
   }
 
   if (q?.height) {
-    size.height = q?.height
+    size.height = q?.height;
   }
 
-  const attrs = { ...attrType[mode], ...size, ...attr }
+  const attrs = { ...attrType[mode], ...size, ...attr };
 
   return Object.keys(attrs)
     .map((key) => `${key}="${attrs[key]}"`)
-    .join(" ")
+    .join(' ');
 }
 
 function getQueryParamsRegex(url) {
-  const query = url.split("?")[1] || ""
-  return query.split("&").reduce((params, param) => {
-    const [key, value] = param.split("=")
-    params[decodeURIComponent(key)] = decodeURIComponent(value || "")
-    return params
-  }, {})
+  const query = url.split('?')[1] || '';
+  return query.split('&').reduce((params, param) => {
+    const [key, value] = param.split('=');
+    params[decodeURIComponent(key)] = decodeURIComponent(value || '');
+    return params;
+  }, {});
 }
 
 function objectToQueryString(obj) {
-  return new URLSearchParams(obj).toString()
+  return new URLSearchParams(obj).toString();
 }
