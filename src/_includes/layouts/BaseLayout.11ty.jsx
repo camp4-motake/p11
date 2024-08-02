66293b5bda3abd28acb9bbf1efce70b0b3ee51a6
motake <munetoshi_otake@camp4.jp>
2024-08-02 14:23:10 +0900

    update deps

--- src/_includes/layouts/BaseLayout.11ty.jsx
+++ src/_includes/layouts/BaseLayout.11ty.jsx
@@ -18,26 +18,26 @@ export function BaseLayout(data) {
 
   return (
     <>
-      <html lang={metadata.lang} className='no-js'>
+      <html lang={metadata.lang} className="no-js">
         <head>
           <meta charSet={metadata.encoding} />
           <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0, viewport-fit=cover'
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, viewport-fit=cover"
           />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='theme-color' content='#ffffff' />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="theme-color" content="#ffffff" />
 
           <title>{titleText}</title>
           <meta
-            name='description'
+            name="description"
             content={description || metadata.description || ''}
           />
-          <meta property='og:locale' content='ja_JP' />
-          <meta property='og:type' content={ogType || 'article'} />
-          <meta property='og:title' content={title} />
+          <meta property="og:locale" content="ja_JP" />
+          <meta property="og:type" content={ogType || 'article'} />
+          <meta property="og:title" content={title} />
           <meta
-            property='og:description'
+            property="og:description"
             content={description || metadata.description || ''}
           />
           {/* <meta
@@ -67,11 +67,11 @@ export function BaseLayout(data) {
           /> */}
           {metadata.webFonts && (
             <>
-              <link rel='preconnect' href='https://fonts.googleapis.com' />
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
               <link
-                rel='preconnect'
-                href='https://fonts.gstatic.com'
-                crossOrigin=''
+                rel="preconnect"
+                href="https://fonts.gstatic.com"
+                crossOrigin=""
               />
             </>
           )}
@@ -80,13 +80,13 @@ export function BaseLayout(data) {
               url?.path && (
                 <link
                   key={`fonts-${url?.key}`}
-                  rel='stylesheet'
+                  rel="stylesheet"
                   href={url?.path}
                 />
               ),
           )}
-          <link rel='stylesheet' href='/src/main.css' />
-          <script type='module' src='/src/main.ts'></script>
+          <link rel="stylesheet" href="/src/main.css" />
+          <script type="module" src="/src/main.ts"></script>
         </head>
 
         <body>
--- src/site/index.11ty.jsx
+++ src/site/index.11ty.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BaseLayout } from '../_includes/layouts/BaseLayout.11ty';
 import { Sample } from '../_includes/components/Sample';
+import { BaseLayout } from '../_includes/layouts/BaseLayout.11ty';
 
 export const data = {
   title: 'title text',
@@ -12,8 +12,8 @@ export default function render(data) {
     <BaseLayout {...data}>
       <h1>{data.title}</h1>
       <p>text</p>
-      <div x-data='inView' x-bind='trigger'></div>
-      <Sample label='sample component text' />
+      <div x-data="inView" x-bind="trigger"></div>
+      <Sample label="sample component text" />
     </BaseLayout>
   );
 }
