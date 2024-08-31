d399374dd385cfd7cacc8918a033c7622652fda0
motake <munetoshi_otake@camp4.jp>
2024-08-31 16:34:50 +0900

    remove

--- src/pages/index.11ty.jsx
+++ src/pages/index.11ty.jsx
@@ -13,7 +13,6 @@ export default function render(data) {
       <p>text</p>
       <div x-data="inView" x-bind="trigger"></div>
       <img src="/src/images/test300x300.png?as=webp" alt="" />
-      <img src="/src/images/test300x300.png?as=webp&width=100" alt="" />
       <Sample label="sample component text" />
     </BaseLayout>
   );
