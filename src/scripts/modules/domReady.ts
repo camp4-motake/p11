b775eafabd847c9ed6928222a7033d5787a5172b
motake <munetoshi_otake@camp4.jp>
2024-10-02 09:31:14 +0900

    add modules

---
+++ src/scripts/modules/domReady.ts
@@ -0,0 +1,9 @@
+export function domReady(): Promise<void> {
+  return new Promise<void>((resolve) => {
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', () => resolve());
+    } else {
+      resolve();
+    }
+  });
+}
